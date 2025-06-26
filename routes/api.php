af25f2f9f15bfa9d406237e1f3f71c695eece6c9
WildF20 <wildanfebrian9@gmail.com>
2025-06-26 11:43:24 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
+Route::namespace('App\Http\Controllers\API')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('/token', 'AuthController@generateToken');
     });
