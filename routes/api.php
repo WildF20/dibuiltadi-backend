74c1a7135bf6c2d9cceba3fe7ba378103d4b12dd
WildF20 <wildanfebrian9@gmail.com>
2025-06-26 11:41:52 +0700

    Simplied routing for better maintenance and folder management

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-Route::prefix('auth')->group(function () {
-    Route::post('/token', [AuthController::class, 'generateToken']);
-});
-
-Route::middleware(['api.auth'])->group(function () {
-    Route::post('/auth/revoke', [AuthController::class, 'revokeTokens']);
+Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/token', 'AuthController@generateToken');
+    });
+    
+    Route::middleware(['api.auth'])->group(function () {
+        Route::post('/auth/revoke', 'AuthController@revokeTokens');
+    });
 });
